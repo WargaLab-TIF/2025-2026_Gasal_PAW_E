79569778d640d28227370281b448ef2d21ec9607
umam-cloud <fatihulumam1617@gmail.com>
2025-10-03 17:33:02 +0700

    nugas

--- TM-1/PAW2025-1_E_24-209_M._fatihul_Umam/index.php
+++ TM-1/PAW2025-1_E_24-209_M._fatihul_Umam/index.php
@@ -8,46 +8,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="ytta.css">
 </head>
 <body>
-    <div class="form-container">
-    <h2>Membuat Table</h2>
-    <form action="index.php" method="POST">
-      <div class="form-group">
-        <label for="ukuran">Batas Ukuran</label>
-        <input type="number" id="ukuran" name="ukuran" placeholder="Masukkan ukuran table" required>
+    <div class="container">
+      <div class="form-container">
+        <h2>Membuat Table</h2>
+        <form action="index.php" method="POST">
+          <div class="form-group">
+            <label for="ukuran">Batas Ukuran</label>
+            <input type="number" id="ukuran" name="ukuran" placeholder="Masukkan ukuran table" required>
+          </div>
+          <div class="form-group">
+            <label for="aturan">Aturan filter</label>
+            <select id="aturan" name="aturan" required>
+              <option value="">-- Pilih --</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="warna">Warna filter</label>
+            <select id="warna" name="warna" required>
+              <option value="">-- Pilih --</option>
+              <option value="Red">Red</option>
+              <option value="Blue">Blue</option>
+              <option value="Green">Green</option>
+            </select>
+          </div>
+          <button type="submit">Kirim Data</button>
+        </form>
       </div>
-      <div class="form-group">
-        <label for="aturan">Aturan filter</label>
-        <select id="aturan" name="aturan" required>
-          <option value="">-- Pilih --</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="warna">Warna filter</label>
-        <select id="warna" name="warna" required>
-          <option value="">-- Pilih --</option>
-          <option value="Red">Red</option>
-          <option value="Blue">Blue</option>
-          <option value="Green">Green</option>
-        </select>
-      </div>
-      <button type="submit">Kirim Data</button>
-    </form>
-  </div>
-  <div class="form-container"> 
-    <?php 
-      $ukuran = $_POST['ukuran'];
-      $aturan = $_POST['aturan'];
-      $warna = $_POST['warna'];
+        <?php 
+          $ukuran = $_POST['ukuran'];
+          $aturan = $_POST['aturan'];
+          $warna = $_POST['warna'];
 
-      echo Table($ukuran, $aturan, $warna);
-    ?>
-  </div>
+          echo Table($ukuran, $aturan, $warna);
+        ?>
+    </div>
 </body>
 </html>
