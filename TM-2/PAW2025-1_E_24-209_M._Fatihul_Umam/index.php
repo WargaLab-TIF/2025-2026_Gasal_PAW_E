fd3f87a03f11ec9874921a0fb2585d23316e7f2a
umam-cloud <fatihulumam1617@gmail.com>
2025-10-09 14:10:34 +0700

    mulai nugas

---
+++ TM-2/PAW2025-1_E_24-209_M._Fatihul_Umam/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PAW2025-1_E_24-209_M._Fatihul_Umam</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>umam</h1>
+</body>
+</html>
