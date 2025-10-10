bd4934658e41f637e10a30aee68477628d305d8a
hadinuryani <wongsedah6@gmail.com>
2025-10-10 08:25:14 +0700

    menyelesaikan TM 2

---
+++ TM-2/PAW2025-1_E_24-135_Ahmad_Hadi_Nuryani/function.php
@@ -0,0 +1,31 @@
+<?php
+// Membersihkan input dari karakter berbahaya
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+function is_alfabate($data) {
+    return preg_match("/^[a-zA-Z\s']+$/", $data);
+}
+
+function is_number($data) {
+    return preg_match("/^[0-9]+$/", $data);
+}
+
+function is_alphanumeric($data) {
+    return preg_match("/^[a-zA-Z0-9\s]+$/", $data);
+}
+
+function is_valid_length_numeric($data, $min, $max) {
+    $len = strlen($data);
+    return ($len >= $min && $len <= $max);
+}
+
+function is_valid_length_string($data, $min, $max) {
+    $len = strlen($data);
+    return ($len >= $min && $len <= $max);
+}
+?>
