14d98ba8d57e1bb4bc02da382b2b590137bef303
Ryan <64879948+YannYam@users.noreply.github.com>
2025-10-10 20:20:38 +0700

    Add files via upload

---
+++ TM-2/PAW2025-1_E_24-163_Ryan_Hidayat/fungsi.php
@@ -0,0 +1,33 @@
+<?php 
+	function required($data) {
+		return empty($data);
+	}
+
+	function alfabet($data) {
+		return preg_match("/^[a-zA-Z\s]+$/", $data);
+	}
+
+	function alfabet_batas($data){
+		return preg_match("/^[a-zA-Z\s]{3,}$/", $data);
+	}
+
+	function numerik_telepon($data) {
+		return preg_match("/^(08|628|\+628)[0-9]{9,10}+$/", $data);
+	}
+
+	function numerik($data) {
+		return preg_match("/^[0-9]+$/", $data);
+	}
+
+	function numerik_harga($data) {
+		return preg_match("/^[0-9]{7,}$/", $data);
+	}
+
+	function alfanumerik($data) {
+		return preg_match("/^[a-zA-Z0-9]{8}$/", $data);
+	}
+
+	function is_alamat($data){
+		return preg_match("/^[a-zA-Z\s.,]+$/", $data);
+	}
+?>
