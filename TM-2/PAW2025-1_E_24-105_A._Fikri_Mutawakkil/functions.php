70fc274c36f19a26394d90610be2d8b23338c1a3
240411100105-design <240411100105@student.trunojoyo.ac.id>
2025-10-11 18:31:44 +0700

    Add files via upload

---
+++ TM-2/PAW2025-1_E_24-105_A._Fikri_Mutawakkil/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+function test_input($data){
+	$data = trim($data);
+	$data = stripcslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+function wajib_isi($data){
+	return !empty($data);
+}
+function alphabet($data){
+	return preg_match("/^[a-zA-Z\s]+$/", $data);
+}
+function alfanumerik($data){
+	return preg_match("/^[a-zA-Z0-9\s]{6,}$/", $data);
+}
+function numerik($data){
+	return preg_match("/^[0-9]{14}$/",$data);
+}
+
+function desimal($data){
+	return ctype_digit($data);
+}
+
+function luas($data){
+	return is_numeric($data);
+}
+
+?>
